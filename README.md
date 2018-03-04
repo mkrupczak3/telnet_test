4b5b49fd8d1542cf407babb218b4da5904e86d05
Krupczak, Matthew William <mkupczak3@gatech.edu>
2018-03-04 01:33:40 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a very basic python script which is supposed to send a uniform telnet command to a uniform port number across multiple adresses.
+
+Usage:
+fruitful.py port command ipaddress1 ipaddress2 ...
+
+e.g. fruitful.py 23 hello 127.0.0.1 8.8.8.8 10.0.0.1
---
+++ fruitful.py
@@ -0,0 +1,33 @@
+import sys, traceback, telnetlib, time
+
+def main():
+    if len(sys.argv) < 5:
+        print("Invalid Arguments, instead use: fruitful.py port command address1 adress2 ...")
+
+    path_of_script = sys.argv[0]
+    targetPort = sys.argv[1]
+    commandToSend = sys.argv[2]
+
+    ip_list = []
+    for index in range(3, len(sys.argv)):
+        ip_list.append(sys.argv[index])
+
+    global tn
+
+    tn = telnetlib.Telnet()
+
+    def sendCommand(host_address, port, command):
+        tn.open(str(host_address), int(port), 5)
+        tn.write(command.encode("ascii")+b"\r\n")
+        tn.close()
+
+    for aHost in ip_list:
+        try:
+            sendCommand(aHost, targetPort, commandToSend)
+        except:
+            print("Exception sending: " + str(commandToSend) + " to: " + str(aHost) + ":" + str(targetPort))
+            continue
+
+if __name__=="__main__":
+    print("main")
+    main()
